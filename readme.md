4ecd1cc7d45729bbebee83bd9b22706c80ae71be
valaxy <valaxy@qq.com>
2015-12-11 22:29:40 +0800

    finish predict function

--- readme.md
+++ readme.md
@@ -5,3 +5,4 @@ Some Alg about parser.
 # Alg List
 - calc first by productions
 - calc follow by productions
+- calc predict table by productions
--- test/predict.js
+++ test/predict.js
@@ -2,16 +2,47 @@ define(function (require) {
 	var pcStore = require('./pc-store'),
 	    predict = require('cjs!../dist/predict')
 
-
 	QUnit.module('predict')
 
-	QUnit.test('sample 1', function (assert) {
+	QUnit.test('sample1', function (assert) {
 		var pc = pcStore.sample1()
 		var pt = predict(pc, 'E')
 		assert.deepEqual(pt.toJSON(), {
-			E: {'id': pc.getProductionsBySymbol('E')[0]},
-			T: {'id': pc.getProductionsBySymbol('T')[0]},
-			F: {'+': pc.getProductionsBySymbol('F')[0], '1': pc.getProductionsBySymbol('F')[1]}
+			E: {'id': pc.getProductionAt(0)},
+			T: {'id': pc.getProductionAt(1)},
+			F: {'+': pc.getProductionAt(2), '1': pc.getProductionAt(3)}
 		})
 	})
+
+	QUnit.test('sample2', function (assert) {
+		var pc = pcStore.sample2()
+		var pt = predict(pc, 'E')
+		assert.deepEqual(pt.toJSON(), {
+			E : {
+				'id': pc.getProductionAt(0),
+				'(' : pc.getProductionAt(0)
+			},
+			EE: {
+				'+': pc.getProductionAt(1),
+				')': pc.getProductionAt(2),
+				'1': pc.getProductionAt(2)
+			},
+			T : {
+				'id': pc.getProductionAt(3),
+				'(' : pc.getProductionAt(3)
+			},
+			TT: {
+				'+': pc.getProductionAt(5),
+				'*': pc.getProductionAt(4),
+				')': pc.getProductionAt(5),
+				'1': pc.getProductionAt(5)
+			},
+			F : {
+				'id': pc.getProductionAt(7),
+				'(' : pc.getProductionAt(6)
+			}
+		})
+	})
+
+
 })
