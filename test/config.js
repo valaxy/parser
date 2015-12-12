ad0611e033538a25b6253889e0074e633fbc3c21
valaxy <valaxy@qq.com>
2015-12-12 15:27:56 +0800

    remove bower

--- test/config.js
+++ test/config.js
@@ -1,10 +1,8 @@
 requirejs.config({
 	baseUrl: '../',
 	paths  : {
-		cjs         : 'bower_components/cjs/cjs',
-		'amd-loader': 'bower_components/amd-loader/amd-loader',
-		es6         : 'bower_components/requirejs-babel/es6',
-		babel       : 'bower_components/requirejs-babel/babel-5.8.22.min',
+		cjs         : 'node_modules/cjs/cjs',
+		'amd-loader': 'node_modules/amd-loader/amd-loader',
 		underscore  : 'node_modules/underscore/underscore'
 	}
 })
